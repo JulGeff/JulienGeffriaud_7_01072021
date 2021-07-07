32e6d9022ba564c19847714e255542f6e2dcb9ef
JulGeff <julien.geffriaud.pro@gmail.com>
2021-07-07 14:39:01 +0200

    utilisation state et axios pour formulaires user

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,62 +1,57 @@
 import loginpic from "../assets/login-image.png";
 import '../styles/LoginSignup.css'
 import React from "react";
+import axios from "axios";
 import {
     Link,
   } from "react-router-dom";
 
-function handleLogin(e) {
-    e.preventDefault();
-
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-  
-    let user = {
-    email : email,
-    password : password,
-    }
-        
-        console.log(user);
-
-        let sendOptions = {
-            method: 'POST',
-            body: JSON.stringify(user),
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8'
-            },
-        }
-
-        fetch("http://localhost:3000/api/auth/login", sendOptions)
-            .then((response) => response.json())
-            .then((json) => {
-                console.log(json)
-                }
-                
-            )
-            .catch(x => { // gestion des erreurs en cas de fail d'API
-                console.log(x);
-            })
-
-}
 
 function Login() {
-    return  (
-        <div className='loginsignup'> 
-            <img src={loginpic} alt='Groupomania' className='loginpic' />
-            <h1>Connectez-vous</h1>
-            <form className="logform"  onSubmit={handleLogin}>
-                <label>email
-                    <input id="email" className="input" type="email" name="email" autoComplete="username"/>
-                </label>
-                <label>mot de passe
-                    <input id="password" className="input" type="password" name="password" autoComplete="current-password" />
-                </label>
-                <input  className="button" type="submit" value="Connexion" />
-            </form>
-            <p>C'est votre première visite ?</p>
-            <p className="changelog"><Link to="/auth/signup">Créez un compte</Link></p>
-        </div>        
-        )
-}
+    const [email, setEmail] = React.useState("");       //initialisation du state vide
+    const [password, setPassword] = React.useState(""); //initialisation du state vide
+  
+    const handleLogin = (event) => {
+        event.preventDefault();
 
+        let loginForm = document.getElementById('loginForm');
+        let loginFormData = new FormData(loginForm);
+        for (var value of loginFormData.values()) { // affiche les 2 valeurs (emai, password) du FormData créé dans la console
+            console.log(value);
+         }
+ 
+        axios({
+            method: "post",
+            url: "http://localhost:3000/api/auth/signup",
+            data: loginFormData,
+            headers: { "Content-Type": "multipart/form-data" },
+          })
+            .then(function (response) {
+              console.log(response);
+            })
+            .catch(function (response) {
+              console.log(response);
+            });
+
+    }
+
+    return (
+        <div className='loginsignup'> 
+        <img src={loginpic} alt='Groupomania' className='loginpic' />
+        <h1>Connectez-vous</h1>
+        <form id="loginForm" className="logform"  onSubmit={handleLogin}>
+            <label>email
+                <input id="email" className="input" type="email" name="email" autoComplete="username" value={email} onChange={e => setEmail(e.target.value)}/>
+            </label>
+            <label>mot de passe
+                <input id="password" className="input" type="password" name="password" autoComplete="current-password" value={password} onChange={e => setPassword(e.target.value)}/>
+            </label>
+            <input  className="button" type="submit" value="Connexion" />
+        </form>
+        <p>C'est votre première visite ?</p>
+        <p className="changelog"><Link to="/auth/signup">Créez un compte</Link></p>
+    </div>   
+    );
+  }
+  
 export default Login
