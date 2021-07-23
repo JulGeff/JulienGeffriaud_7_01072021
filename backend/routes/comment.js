2cc232ed986659a78eb453dc4d282ee9190a03e2
JulGeff <julien.geffriaud.pro@gmail.com>
2021-07-23 14:57:02 +0200

    sass setup

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,15 +3,13 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth'); // importation middleware d'authentification
-const publicationCtrl = require('../controllers/comment')           // Appel des controllers
+const commentCtrl = require('../controllers/comment')           // Appel des controllers
 
 
-router.post('/', auth, publicationCtrl.createComment);               // Création d'un commentaire
-router.get('/', auth, publicationCtrl.getAllComments);               // Récupèration toutes les publications
-//router.delete('/publication', publicationCtrl.deletePublication);         // Suppression d'une publication
-
-//router.get('/', publicationCtrl.getUserPublications);             // Récupération d'une publication
-//router.put('/publication', publicationCtrl.modifyPublication);            // Modification d'une publication
+router.post('/', auth, commentCtrl.createComment);               // Création d'un commentaire
+router.get('/', auth, commentCtrl.getAllComments);               // Récupèration des 
+//router.delete('/', auth, commentCtrl.deleteComment);         // Suppression d'une publication
+//router.put('/publication', publicationCtrl.modifyPublication);            // Modification d'un commentaire
 
 
 
