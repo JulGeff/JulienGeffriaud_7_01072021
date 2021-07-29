54ddc7d503a380f0a16ff993681c1e6800b161d6
JulGeff <julien.geffriaud.pro@gmail.com>
2021-07-29 07:34:08 +0200

    put auth middleware back on all routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -6,9 +6,9 @@ const auth = require('../middleware/auth'); // importation middleware d'authenti
 const commentCtrl = require('../controllers/comment')// Appel des controllers
 
 
-router.post('/', commentCtrl.createComment);               // Création d'un commentaire
-router.get('/', commentCtrl.getAllComments);               // Récupèration des commentaires associés à une publication
-router.delete('/', commentCtrl.deleteComment);             // Suppression d'un commentaire
+router.post('/', auth, commentCtrl.createComment);               // Création d'un commentaire
+router.get('/', auth, commentCtrl.getAllComments);               // Récupèration des commentaires associés à une publication
+router.delete('/', auth, commentCtrl.deleteComment);             // Suppression d'un commentaire
 
 module.exports = router; // export pour pouvoir utiliser dans app.js
 
