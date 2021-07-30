0dad863271799d170f535972fd4762bd7fa6f0a9
JulGeff <julien.geffriaud.pro@gmail.com>
2021-07-30 12:26:49 +0200

    add focus on delete buttons with tabindex

--- frontend/src/components/publications/SelectedPublication.js
+++ frontend/src/components/publications/SelectedPublication.js
@@ -204,7 +204,7 @@ function SelectedPublication() {
 
           {/* si user connecté est l'auteur de la publication ou admin, on affiche un lien pour la supprimer*/} 
           {selectedPublication.userId===user.id || user.isAdmin
-                  ? (<div className = "publication__icons__delete" onClick = {e => {if (window.confirm('Etes-vous sûr·e de vouloir supprimer cette publication ?')) handleDelete(e, selectedPublication.id)}} >
+                  ? (<div tabindex="0" className = "publication__icons__delete" onClick = {e => {if (window.confirm('Etes-vous sûr·e de vouloir supprimer cette publication ?')) handleDelete(e, selectedPublication.id)}} >
                       <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#E02F04" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
                     </div>) 
                   : ('')}
@@ -244,7 +244,7 @@ function SelectedPublication() {
               
               {/* si user connecté est l'auteur du commentaire ou admin, on affiche un lien pour le supprimer*/} 
                {item.userId===user.id || user.isAdmin
-                ? (<div  onClick = {e => {if (window.confirm('Etes-vous sûr·e de vouloir supprimer ce commentaire ?')) handleDeleteComment(e, item.id)}} >
+                ? (<div tabindex="0" onClick = {e => {if (window.confirm('Etes-vous sûr·e de vouloir supprimer ce commentaire ?')) handleDeleteComment(e, item.id)}} >
                     <svg className = "publication__displaycomments__list__delete" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#E02F04" d="M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z"></path></svg>
                 </div>) 
                 : ('')}
